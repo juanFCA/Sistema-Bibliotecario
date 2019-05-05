2f857cfbdedc86729d3059a0909dd0c2e4a2893e
Juan Ferreira Carlos <juanfcarlos.93@gmail.com>
2019-05-05 10:30:22 -0300

    Composer
    Adicionado Sistema para uso de Bibliotecas/Libraries via composer

---
+++ Biblioteca/exemplares.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "view/template.php";
+require_once "db/conexao.php";
+require_once "dao/exemplarDAO.php";
+require_once "modelo/exemplar.php";
+require_once "dao/livroDAO.php";
+require_once "modelo/livro.php";
+
+
+?>
